513e0b9c51990e9c0c3bf4fd96df7e0ef1dc62ba
Alex White <devalexwhite@gmail.com>
2020-05-23 09:52:44 -0400

    Attempting to fix Nuxt Content in prod

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,14 @@ export default {
   modules: [
     '@nuxt/content'
   ],
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
   /*
   ** Build configuration
   */
