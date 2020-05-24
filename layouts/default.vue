13e49959101df696264146c9256523971ccddb50
Alex White <devalexwhite@gmail.com>
2020-05-24 08:43:36 -0400

    Navigation reflects route

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,12 @@
     <nav class="w-full justify-start items-center flex flex-col md:flex-row py-6">
       <logo width="80" />
       <ul class="flex flex-row px-10 overflow-x-auto">
-        <li v-for="link in navigation" :key="link" class="text-lg font-semibold text-gray-700 hover:text-primary px-4 lowercase">
-          <nuxt-link :to="link.href">
-            {{ link.label }}
-          </nuxt-link>
+        <li
+          v-for="link in navigation"
+          :key="link"
+          :class="`text-lg pb-1 font-semibold text-gray-700 hover:text-primary px-4 lowercase  ${isCurrent(link.view) && 'border-b-2 border-primary text-primary'}`"
+        >
+          <nuxt-link :to="link.href">{{ link.label }}</nuxt-link>
         </li>
       </ul>
     </nav>
@@ -21,16 +23,18 @@ export default {
   components: {
     Logo
   },
-  data () {
+  data() {
     return {
       navigation: [
         {
           label: 'home',
-          href: '/'
+          href: '/',
+          view: 'index'
         },
         {
           label: 'events',
-          href: '/events'
+          href: '/events',
+          view: ['events', 'events-id']
         }
       ]
     }
@@ -38,6 +42,14 @@ export default {
   head() {
     return {
       title: 'Front End Columbus'
+    }
+  },
+  methods: {
+    isCurrent(view) {
+      const currentView = this.$nuxt.$route.name
+
+      if (typeof view === 'string') return view === currentView
+      else return view.includes(currentView)
     }
   }
 }
