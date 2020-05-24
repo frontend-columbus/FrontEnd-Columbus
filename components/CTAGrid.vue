910133d88f869c97d937654c852059a40e0af1b6
Alex White <devalexwhite@gmail.com>
2020-05-23 20:03:13 -0400

    Adding homepage CTA grid

---
+++ components/CTAGrid.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ctaGrid grid grid-cols-1 md:grid-cols-2 max-w-lg mx-auto gap-6 py-8">
+    <div class="border-4 justify-center border-gray-500 py-2 px-6 shadow relative flex justify-center items-center flex-col cursor-pointer ease-in transition duration-200 hover:shadow-lg" v-for="{ header, subheader, link} in squares" :key="subheader">
+      <nuxt-link :to="link">
+        <div
+          class="absolute top-0 right-0 pt-2 pr-2 text-primary"
+          v-html="feather.icons['arrow-right'].toSvg({ 'stroke-width': '4px', width: '30px', height: '30px'})"
+        />
+        <h3 class="font-extrabold text-6xl text-gray-800" v-if="header">{{ header }}</h3>
+        <h4 class="text-xl font-extrabold uppercase text-gray-700" v-if="subheader">{{ subheader }}</h4>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import feather from 'feather-icons'
+
+export default {
+  data() {
+    return {
+      feather,
+      squares: [
+        {
+          header: '5',
+          subheader: 'upcoming events',
+          link: '/'
+        },
+        {
+          header: '10',
+          subheader: 'community job postings',
+          link: '/'
+        },
+        {
+          header: '5',
+          subheader: 'members in discord',
+          link: '/'
+        },
+        {
+          subheader: "we're looking for speakers!",
+          link: '/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+.ctaGrid {
+  grid-auto-rows: 1fr;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,36 @@
   <div class="py-6 md:py-16">
     <header class="flex flex-col items-center">
       <!-- <logo /> -->
-      <h1 class="font-extrabold text-4xl md:text-5xl uppercase tracking-wider text-center">Front End Columbus</h1>
+      <h1
+        class="font-extrabold text-4xl md:text-5xl uppercase tracking-wider text-center"
+      >Front End Columbus</h1>
       <div class="py-4">
         <p
           class="max-w-lg text-center text-lg font-medium leading-relaxed text-gray-600 tracking-wide"
         >Columbus based Front End development community, meetup and more.</p>
       </div>
     </header>
+    <div class="py-8">
+      <cta-grid />
+    </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import CtaGrid from '~/components/CTAGrid.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    CtaGrid
   }
 }
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 h1 {
-  background: linear-gradient(114.63deg, #FFB99B 47.46%, #FFCCB6 76.87%);
+  background: linear-gradient(114.63deg, #ffb99b 47.46%, #ffccb6 76.87%);
   background-clip: text;
   -webkit-text-fill-color: transparent;
 }
