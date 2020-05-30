0753c6088e6aca9c0f857793acdb6dde2b2dc619
Alex White <devalexwhite@gmail.com>
2020-05-30 08:57:04 -0400

    Fixing core js build issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,20 @@ export default {
     /*
     ** You can extend webpack config here
     */
+    babel: {
+      presets({ isServer }) {
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              corejs: { version: 3 }
+            }
+          ]
+        ]
+      }
+    },
     extend(config, ctx) {
     }
   }
