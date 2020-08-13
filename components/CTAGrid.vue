d586812c55030f145280159704be112e2fc1d67c
Alexander White <devalexwhite@gmail.com>
2020-08-13 09:22:57 -0400

    Update CTAGrid.vue

--- components/CTAGrid.vue
+++ components/CTAGrid.vue
@@ -44,7 +44,7 @@ export default {
           link: '/jobs'
         },
         {
-          header: '37',
+          header: '42',
           subheader: 'members in discord',
           link: '/community'
         },
