b40e01633c97a3e00ae3803975be206be2f38531
Alex White <alex@meetthe.dev>
2021-02-26 10:18:00 -0500

    Trying to fix content gen

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 
 export default {
-  mode: 'spa',
+  ssr: 'false',
   target: 'static',
   /*
   ** Headers of the page
@@ -56,14 +56,6 @@ export default {
   modules: [
     '@nuxt/content'
   ],
-  generate: {
-    async routes() {
-      const { $content } = require('@nuxt/content')
-      const files = await $content().only(['path']).fetch()
-
-      return files.map(file => file.path === '/index' ? '/' : file.path)
-    }
-  },
   /*
   ** Build configuration
   */
